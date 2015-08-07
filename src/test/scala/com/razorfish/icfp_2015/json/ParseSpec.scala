7a7e24c2cc47195835c1ddc4f0b018816aba40d5
Steven Skelton <steven.skelton@nurun.com>
2015-08-07 16:07:17 -0400

    problem maps

--- src/test/scala/com/razorfish/icfp_2015/json/ParseSpec.scala
+++ src/test/scala/com/razorfish/icfp_2015/json/ParseSpec.scala
@@ -31,12 +31,28 @@ class ParseSpec extends org.specs2.mutable.Specification {
       input.filled must have size 45
     }
 
+    /*
     "board" in {
       val board = Parse.createBoard(file)
       board.print()
       success
-
     }
+    */
   }
 
+  val problems = (0 to 23).map(i => new File(s"src/test/resources/problems/problem_$i.json"))
+
+  "all problems" in {
+    "parse" in {
+      problems.forall {
+        case file =>
+          val board = Parse.createBoard(file)
+          println("----------------------------------------------------")
+          println(file.getPath)
+          println("----------------------------------------------------")
+          board.print()
+          true
+      } must beTrue
+    }
+  }
 }
