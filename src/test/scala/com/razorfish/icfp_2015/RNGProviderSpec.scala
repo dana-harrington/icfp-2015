ffd08ea69fcf648b03ba2278a4438dfa84e83d6b
Daniel Vasilev <daniel.vasilev@nurun.com>
2015-08-07 14:39:59 -0400

    Tests class for NRG added

---
+++ src/test/scala/com/razorfish/icfp_2015/RNGProviderSpec.scala
@@ -0,0 +1,11 @@
+package com.razorfish.icfp_2015
+
+import org.specs2.mutable.Specification
+
+class RNGProviderSpec extends Specification {
+  "RNGP" should {
+    "" in {
+      1 must beEqualTo(1)
+    }
+  }
+}
