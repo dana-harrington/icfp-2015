0d75f76a2f59f316355e689422b88bb0c8226524
Dana Harrington <dana.g.harrington@gmail.com>
2015-08-07 15:29:07 -0400

    - implemented cell rotations

--- src/test/scala/com/razorfish/icfp_2015/models/CellTest.scala
+++ src/test/scala/com/razorfish/icfp_2015/models/CellTest.scala
@@ -11,11 +11,25 @@ class CellTest extends org.specs2.mutable.Specification {
       c.translate(W) === Cell(0, 3)
     }
 
+
     "rotate around a pivot point" in {
+      val p = Cell(2, 5)
+      val cubeVector = c.cubeCoords - p.cubeCoords
+      cubeVector === CubeCoord(0,2,-2)
       c.rotate(CW, Cell(2, 5)) === Cell(3, 3)
+    }
+
+    "be unchanged by conversions to cube coordinates and back" in {
+      c.cubeCoords.cell == c
     }
   }
 
+  "A CubeCoordinate" should {
+    val cc = CubeCoord(3, 0, -3)
+    "rotate about the 0 point" in {
+      cc.rotate(CW) === CubeCoord(3, -3, 0)
+    }
+  }
 
 
 }
