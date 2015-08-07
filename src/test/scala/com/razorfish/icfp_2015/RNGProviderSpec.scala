7a3ef6bb663ff301580bac300ba4187a0a7ca2ef
Daniel Vasilev <daniel.vasilev@nurun.com>
2015-08-07 14:40:11 -0400

    Tests class for NRG added

--- src/test/scala/com/razorfish/icfp_2015/RNGProviderSpec.scala
+++ src/test/scala/com/razorfish/icfp_2015/RNGProviderSpec.scala
@@ -1,10 +1,18 @@
 package com.razorfish.icfp_2015
 
 import org.specs2.mutable.Specification
+import scala.math.pow
 
 class RNGProviderSpec extends Specification {
   "RNGP" should {
-    "" in {
+    "return the sequence" in {
+      val m = pow(2, 32).toLong
+      val a = 1103515245
+      val c = 12345
+      val seed = 17
+
+      val rngp = new RNGProvider(m, a, c)
+
       1 must beEqualTo(1)
     }
   }
