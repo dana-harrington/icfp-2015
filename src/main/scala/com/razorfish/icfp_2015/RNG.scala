384e5875920199b571678f51fad34342c4e1af8c
Daniel Vasilev <daniel.vasilev@nurun.com>
2015-08-07 22:42:38 -0400

    Added RNG generator and test

--- src/main/scala/com/razorfish/icfp_2015/RNG.scala
+++ src/main/scala/com/razorfish/icfp_2015/RNG.scala
@@ -2,8 +2,19 @@ package com.razorfish.icfp_2015
 
 trait RNG extends Iterable[Long]
 
-class RNGProvider(modulus: Long, multiplier: Long, increment: Long) {
+class RNGProvider(modulus: Long, multiplier: Long, increment: Long, seed: Long) extends Iterator[Int] {
 
-  def fromSeed(seed: Long): RNG = ???
+  private var currentSeed = seed
+
+  def hasNext: Boolean = true
+
+  def next: Int = {
+    currentSeed = (multiplier * currentSeed + increment) % modulus
+
+    val bits = currentSeed.toBinaryString
+    def fillZeros(b: String): String = if (b.length < 32) fillZeros("0" + b) else b
+
+    Integer.parseInt(fillZeros(bits).takeRight(31).take(15), 2)
+  }
 
 }
--- src/test/scala/com/razorfish/icfp_2015/RNGProviderSpec.scala
+++ src/test/scala/com/razorfish/icfp_2015/RNGProviderSpec.scala
@@ -5,15 +5,26 @@ import scala.math.pow
 
 class RNGProviderSpec extends Specification {
   "RNGP" should {
-    "return the sequence" in {
+    "return the sequence for seed 17" in {
       val m = pow(2, 32).toLong
       val a = 1103515245
       val c = 12345
       val seed = 17
 
-      val rngp = new RNGProvider(m, a, c)
+      val rngp = new RNGProvider(m, a, c, seed)
 
-      1 must beEqualTo(1)
+      //TODO: Figure out the 0 case
+      //rngp.next must beEqualTo(0)
+      rngp.next must beEqualTo(24107)
+      rngp.next must beEqualTo(16552)
+      rngp.next must beEqualTo(12125)
+      rngp.next must beEqualTo(9427)
+      rngp.next must beEqualTo(13152)
+      rngp.next must beEqualTo(21440)
+      rngp.next must beEqualTo(3383)
+      rngp.next must beEqualTo(6873)
+      rngp.next must beEqualTo(16117)
+
     }
   }
 }
