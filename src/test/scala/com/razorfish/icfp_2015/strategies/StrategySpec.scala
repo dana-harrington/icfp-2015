f90f1dfdb2980e5129aa1c67ad16f4a72673d488
Steven Skelton <steven.skelton@nurun.com>
2015-08-08 21:55:00 -0400

    add check for icfp solution's score

--- src/test/scala/com/razorfish/icfp_2015/strategies/StrategySpec.scala
+++ src/test/scala/com/razorfish/icfp_2015/strategies/StrategySpec.scala
@@ -5,7 +5,7 @@ import java.io.File
 import org.specs2.matcher.MatchResult
 import org.specs2.mutable.Specification
 
-import com.razorfish.icfp_2015.{Config, GameExecution, MoveEncoder}
+import com.razorfish.icfp_2015.{Score, Config, GameExecution, MoveEncoder}
 import com.razorfish.icfp_2015.json.{Output, Parse}
 import com.razorfish.icfp_2015.models._
 import scala.concurrent.duration._
@@ -25,12 +25,12 @@ trait StrategySpec extends Specification {
     solution.toCharArray.flatMap(getMove(_)).toSeq
   }
 
-  def specOutput(output: Output, parse: Parse, seed: Long, isDebug: Boolean): MatchResult[_] = {
+  def specOutput(output: Output, parse: Parse, seed: Long, expectedScore: Option[Score], isDebug: Boolean): MatchResult[_] = {
     val gameMoves = solutionToGameMoves(output.solution)
     val source = new UnitSource(seed, parse.gameUnits, parse.sourceLength)
 
     var gameConfiguration = GameConfiguration(parse.board, source).asInstanceOf[ActiveGameConfiguration]
-    var gameDone = false
+    var completedGame: Option[CompletedGameConfiguration] = None
 
     if (isDebug) {
       println(s"Game with ${gameMoves.size} moves")
@@ -43,11 +43,11 @@ trait StrategySpec extends Specification {
     for (move <- gameMoves) {
 
       gameConfiguration.update(move) match {
-        case _: CompletedGameConfiguration =>
+        case cgc: CompletedGameConfiguration =>
           moveCount += 1
           if (isDebug) println(s"Move #$moveCount: ${move}: Game over")
-          gameDone = true
-        case gc: ActiveGameConfiguration if !gameDone => gameConfiguration = gc
+          completedGame = Some(cgc)
+        case gc: ActiveGameConfiguration if completedGame.isEmpty => gameConfiguration = gc
           moveCount += 1
           if (isDebug) {
             println(s"Move #$moveCount: ${move}")
@@ -58,16 +58,17 @@ trait StrategySpec extends Specification {
 
     }
 
-    gameDone === true
+    completedGame.isDefined === true
+    if (expectedScore.isDefined) completedGame.map(_.score) === expectedScore
     gameMoves.size === moveCount
   }
 
-  def spec(file: File, strategy: Strategy, config: Config, isDebug: Boolean = true): MatchResult[_] = {
+  def spec(file: File, strategy: Strategy, config: Config, expectedScore: Option[Score] = None, isDebug: Boolean = true): MatchResult[_] = {
     val parse = Parse(file)
     val seed = parse.sourceSeeds.head
     val gameExecution = new GameExecution(strategy, parse, seed, config.timeLimit, config.memoryLimit, config.powerPhrases)
     val output = Await.result(gameExecution.run, 20 seconds)
-    specOutput(output, parse, seed, isDebug)
+    specOutput(output, parse, seed, expectedScore, isDebug)
   }
 }
 
@@ -100,8 +101,9 @@ miipmemimmeeeemimimiipipimmipppimeeemimmpppmmpmeeeeemimmemm""")
 
       val file = new File("src/test/resources/problems/problem_6.json")
       val config = Config(Seq(file), Set("Ei!"), None, None)
+      val expectedScore = Some(3261L)
 
-      specOutput(icfpValidOutput, Parse(file), 0, false)
+      specOutput(icfpValidOutput, Parse(file), 0, expectedScore, false)
     }
 
 
